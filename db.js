63cbf1a865daa085105bcc190cdbda0bbb068a7e
AlexVolyk <85078173+AlexVolyk@users.noreply.github.com>
2021-09-23 18:56:50 +0300

    first commit

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    ssl: false
-})
-module.exports = sequelize;
-
 // const Sequelize = require('sequelize');
 
 // const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     ssl: process.env.ENVIRONMENT === 'development',
-// });
-
+//     ssl: false
+// })
 // module.exports = sequelize;
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'development',
+});
+
+module.exports = sequelize;
